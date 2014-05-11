08804cef7a24186d986ee24633b627909d94736b
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-05-11 23:39:40 +1000

    Basic demo project working

--- Demo/DemoGame.cs
+++ Demo/DemoGame.cs
@@ -9,5 +9,18 @@ namespace Demo
 
     public class DemoGame : Game
     {
+        public DemoGame()
+        {
+            this.GraphicsDeviceManager = new GraphicsDeviceManager(this);
+        }
+
+        public GraphicsDeviceManager GraphicsDeviceManager { get; set; }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            this.GraphicsDevice.Clear(Color.Red);
+        }
     }
 }
